0ca5210e1f7c9dcf599ddd9f73f53c54e14727f2
storm66-ag <35832163+storm66-ag@users.noreply.github.com>
2018-07-20 11:54:07 +0500

    Update m1_report.py

--- m1_report.py
+++ m1_report.py
@@ -409,7 +409,7 @@ dbCon.close()
 
 receivers = ['alexxgruzdov@gmail.com']
 for r in receivers:
-    os.system(' echo "Письмо создано автоматически" | mutt  -s  "Cеть Спутниковой Связи.Услуги за %s"  %s -a /usr/traffic-counter/tarifs/limit/month_report/reports/%s.xls' % (month,r,date) )
+    os.system(' echo "Письмо создано автоматически" | mutt  -s  "Cеть  Связи.Услуги за %s"  %s -a /usr/traffic-counter/tarifs/limit/month_report/reports/%s.xls' % (month,r,date) )
 os.system(' echo "Отчет успешно создан!" | mutt  -s  "Услуги за %s" 79043828661@sms.ru ' %(month))
 
 print "Hello1"
